2b236a33e74f358bb339925b7e5b91be7023ea64
tyanil <chnellegrcia@gmail.com>
2025-08-08 20:43:01 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -4,15 +4,15 @@ session_start();
 $valid_email = "johndoe@gmail.com";
 $valid_password = "123456";
 
-$email = $_POST ['email'] ?? '';
-$password = $_POST ['paswword'] ?? '';
+$email = $_POST['email'] ?? '';
+$password = $_POST['password'] ?? '';
 
-if ($email === $valid_email && $password === $valid_password){
-    $_SESSION ['user'] = $email;
-    echo "<h2>Welcome, " . htmlspecialchars($enail) . "!</h2>";
-    echo "<p><a href= 'logout.php'>Logout</a></p>";
+if ($email === $valid_email && $password === $valid_password) {
+    $_SESSION['user'] = $email;
+    echo "<h2>Welcome, " . htmlspecialchars($email) . "!</h2>";
+    echo "<p><a href='logout.php'>Logout</a></p>";
 } else {
-    echo "<h2>Login Failed</h2>"
-    echo "<p><a href= 'index.html'> Try Again</a></p>";
+    echo "<h2>Login Failed</h2>";
+    echo "<p><a href='index.html'>Try Again</a></p>";
 }
 ?>
